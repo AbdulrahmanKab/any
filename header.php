2ae90b22c25c002616892a33647416db5238722c
Abdulrahman Kabar <Abdulrahman Kabar@DESKTOP-H1H7FO9>
2019-02-06 01:36:00 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Coursate</title>
+    <link type="text/css" href="css/home.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="images/video-player.png" />
+    <link href="fonts/Fjalla_One/FjallaOne-Regular.ttf" rel="stylesheet">
+    <link href="fonts/Lobster/Lobster-Regular.ttf" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="css/content.css" rel="stylesheet" type="text/css">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body lang="en" class="body">
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php"><img src="images/icons8-study-480.png" alt="home icon" class="homeimage"></a></li>
+            <li>Home</li>
+            <li class="dropdown">Category<div class="dropdown-content">ff</div></li>
+            <li><form>
+                    <button type="submit" class="btn-home">Sign out</button>
+                </form>
+            </li>
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
+<?php
+
+ ?>
---
+++ index.php
@@ -0,0 +1,99 @@
+
+<?php
+include("header.php");
+?>
+<!DOCTYPE html>
+<html>
+<head></head>
+
+
+<body >
+<main style="display: block;">
+<img src="images/kevin-ku-364843-unsplash.jpg" alt="Home photo" width="100%" height="70%" class="homePhoto2">
+ <img src="images/wahid-khene-978156-unsplash.jpg" alt="Home photo" width="100%" height="70%" class="homePhoto">
+<img src="images/man-user.png" alt="person" class="person">
+    <div class="textPerson"> 280</div>
+    <div class="textvideo"> 360</div>
+    <div class="textPerson1"> user</div>
+    <img src="images/youtube.png" alt="video" class="video">
+    <div class="textvideo1"> video</div>
+    <img src="images/jakob-owens-199505-unsplash.jpg" style="visibility:hidden" width="100%" height="2px">
+    <section class="box">
+    <div class="square" >
+       <img src="images/jakob-owens-199505-unsplash.jpg" id="titlephoto" width="100%" height="50%"/>
+       <div class="title">fdsklf fdslfsjl fjlfsj ffsf lkj ljk ljl</div>
+        <div class="desripton">ddddddff f d df dffd  ffds fds fds dddddddddddd</div>
+        </div>
+    <div class="square" >
+        <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+        <div class="title">ffdddd33333333f</div>
+        <div class="desripton">dddddddddd</div>
+    </div>
+    <div class="square" >
+        <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+        <div class="title">ffdddd33333333f</div>
+        <div class="desripton">dddddddddd</div>
+    </div>
+    <div class="square" >
+        <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+        <div class="title">ffdddd33333333f</div>
+        <div class="desripton">dddddddddd</div>
+    </div>
+    <div class="square" >
+        <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+        <div class="title"><p>ffdddd33333333f </p></div>
+        <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+    </div>
+    <div class="square" >
+        <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+        <div class="title"><p>ffdddd33333333f </p></div>
+        <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+    </div>
+        <div class="square" >
+            <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+            <div class="title"><p>ffdddd33333333f </p></div>
+            <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+        </div>
+        <div class="square" >
+            <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+            <div class="title"><p>ffdddd33333333f </p></div>
+            <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+        </div>
+        <div class="square" >
+            <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+            <div class="title"><p>ffdddd33333333f </p></div>
+            <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+        </div>
+
+        <div class="square" >
+            <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+            <div class="title"><p>ffdddd33333333f </p></div>
+            <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+        </div>
+
+        <div class="square" >
+            <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+            <div class="title"><p>ffdddd33333333f </p></div>
+            <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+        </div>
+
+        <div class="square" >
+            <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+            <div class="title"><p>ffdddd33333333f </p></div>
+            <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+        </div>
+
+        <div class="square" >
+            <img src="images/jakob-owens-199505-unsplash.jpg"  width="100%" height="50%"/>
+            <div class="title"><p>ffdddd33333333f </p></div>
+            <div class="desripton"><p>dddddddddd fff kjl kjlfja  ljkaf ljk fffffffffffffffffffffffff </p></div>
+        </div>
+
+</main>
+<footer class="footer">
+<div style="display: inline;text-transform: capitalize;" ><i> Design by: Abdulrahman Kabar </i></div>
+    <img src="images/icons8-facebook-messenger-480.png" alt="messenger"width="5%" height="15%" style="float: right;">
+    <img src="images/icons8-instagram-480.png" alt="instgram" width="5%" height="15%" style="float: right;">
+</footer>
+</body>
+</html>
